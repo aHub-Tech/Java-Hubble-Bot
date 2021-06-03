c483044e073627b466e97696717615d54eaf401b
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-02 21:13:49 -0300

    Adicionado prop de canal para sugestão e instanciando a classe responsável.

--- src/team/hub/discordbot/Main.java
+++ src/team/hub/discordbot/Main.java
@@ -2,12 +2,15 @@ package team.hub.discordbot;
 
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import team.hub.discordbot.actions.SuggestController;
 import team.hub.discordbot.commands.Apresentation;
 
 import javax.security.auth.login.LoginException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
+
+import static team.hub.discordbot.actions.SuggestController.channelSuggestions;
 
 public class Main  {
 
@@ -18,12 +21,18 @@ public class Main  {
 
     public static void main(String[] args) throws IOException, LoginException {
 
+        // Properties File
         Properties props = new Properties();
         props.load(new FileReader("bot.properties"));
+
+        // Main Builder
         JDABuilder builder = JDABuilder.createDefault(props.getProperty("token"));
 
+        // Strings Props
+        channelSuggestions = props.getProperty("channelSuggestions");
 
-        builder.addEventListeners(new Apresentation());
+        // Propriedades do BOT
+        builder.addEventListeners(new Apresentation(), new SuggestController());
         builder.setActivity(Activity.listening("em Hub Tech"));
         builder.build();
     }
