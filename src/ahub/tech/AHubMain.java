1308b0b8bc895501f0b10e1a5d984e7ecd71e81c
Martinfer22 <martinsocial1706@gmail.com>
2022-02-03 22:27:18 -0300

    reduced new instances from main

--- src/ahub/tech/AHubMain.java
+++
@@ -1,7 +0,0 @@
-package ahub.tech;
-
-public class AHubMain {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/tech/ahub/AHubMain.java
@@ -0,0 +1,9 @@
+package tech.ahub;
+
+import tech.ahub.api.DiscordManager;
+
+public class AHubMain {
+    public static void main(String[] args) {
+        new DiscordManager().init();
+    }
+}
